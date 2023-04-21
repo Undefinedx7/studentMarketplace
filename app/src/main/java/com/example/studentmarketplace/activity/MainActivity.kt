96be80af357fde76c6c9f44300c681a620000914
oussama <razzoukoussama10@gmail.com>
2023-04-21 12:36:37 -0300

    sellFragment

--- app/src/main/java/com/example/studentmarketplace/activity/MainActivity.kt
+++ app/src/main/java/com/example/studentmarketplace/activity/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.example.studentmarketplace.activity
 
-import android.content.ContentValues
 import com.example.studentmarketplace.R
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.studentmarketplace.databinding.ActivityMainBinding
-import com.example.studentmarketplace.fragment.CategoryFragment
-import com.example.studentmarketplace.fragment.FavoriteFragment
-import com.example.studentmarketplace.fragment.HomeFragment
-import com.example.studentmarketplace.fragment.ProfileFragment
+import com.example.studentmarketplace.fragment.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,9 +20,10 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.action_home -> replaceFragment(HomeFragment())
-                R.id.action_profile -> replaceFragment(ProfileFragment())
+                R.id.action_profile -> replaceFragment(profileFragment())
                 R.id.action_favorites -> replaceFragment(FavoriteFragment())
                 R.id.action_category -> replaceFragment(CategoryFragment())
+                R.id.action_sell -> replaceFragment(sellFragment())
                 else ->{
 
                 }
